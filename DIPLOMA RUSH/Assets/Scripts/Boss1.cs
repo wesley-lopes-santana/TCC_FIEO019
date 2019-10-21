c7e5d2cfdce8d87d8d17679ed8c3cf2cd0523fb0
Roberto Jesus <roberto.jesus.amaral@gmail.com>
2019-10-21 20:21:38 -0300

    Boss 1 concluido

--- DIPLOMA RUSH/Assets/Scripts/Boss1.cs
+++ DIPLOMA RUSH/Assets/Scripts/Boss1.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class Boss1 : MonoBehaviour
 {
+    public GameObject[] ProxPosicao;
+    private Transform       alvo;
+    private Rigidbody2D     slimeRb;
+    private Animator        slimeAnimator;
+    public  GameObject      HitBox;
+
+    public float velocidade;
+    public bool estaOlhandoEsquerda;
+    private GameController  _GameController;
+
+    private int contador = 0;
+    private int vidaChefe = 3;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        _GameController = FindObjectOfType(typeof(GameController)) as GameController;
+        alvo = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        slimeRb = GetComponent<Rigidbody2D>();
+        slimeAnimator = GetComponent<Animator>();
+        StartCoroutine("mexeChefe");
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = Vector3.MoveTowards(transform.position, ProxPosicao[contador].transform.position, velocidade * Time.deltaTime);
+        StartCoroutine("mexeChefe");
+
+        if (transform.position.x > alvo.position.x && transform.localScale.x < 0){
+            Gira();
+        }else if(transform.position.x < alvo.position.x && transform.localScale.x > 0){
+            Gira();
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.tag == "hitBox")
+        {
+            Destroy(HitBox);
+            _GameController.playSFX(_GameController.sfxEnemyDead, 0.32f);
+            vidaChefe -= 1;
+            if (vidaChefe == 0){
+                Destroy(this.gameObject);
+            }
+            
+        }
+    }
+
+    IEnumerator mexeChefe(){
+        if (transform.position != ProxPosicao[contador].transform.position){
+
+        }else{
+            if (contador == 5){
+                contador = 0;
+            }else{
+                contador = contador + 1;
+            }
+        }
         
+        yield return new WaitForSeconds(1);
+    }
+
+    void Gira()
+    {
+        float x = transform.localScale.x * -1; //Inverte o sinal do Transform -> Scale -> X
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
     }
 }
+
