bf4f08fcdd271fbffea201d786eb463a4d2c9979
Mario <mario-forcelli@hotmail.com>
2019-11-16 20:15:03 -0300

    Fase 4.2 aumentada

---
+++ DIPLOMA RUSH/Assets/Scripts/Destruir.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruir : MonoBehaviour
+    
+{
+    public float tempo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject, tempo);
+        }
+    }
+    
+ }
+
+
+ 
